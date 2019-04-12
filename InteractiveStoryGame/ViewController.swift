91089da8510fc87f8efc63dca1db5cf42a81dbb1
Bobby Conti <No Email>
2019-04-12 17:16:37 -0400

    Added name text field and error handling

--- InteractiveStoryGame/ViewController.swift
+++ InteractiveStoryGame/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,14 +19,28 @@ class ViewController: UIViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "startAdventure" {
-            guard let pageController = segue.destination as? PageController else {
-                return
-            }
             
-            pageController.page = Adventure.story
+            do {
+                if let name = nameTextField.text {
+                    if name == "" {
+                        throw AdventureError.nameNotProvided
+                    } else {
+                        guard let pageController = segue.destination as? PageController else { return }
+                        
+                        pageController.page = Adventure.story(withName: name)
+                    }
+                }
+            } catch AdventureError.nameNotProvided {
+                let alertController = UIAlertController(title: "Name Not Provided", message: "Provide a name to start the story", preferredStyle: .alert)
+                
+                let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alertController.addAction(action)
+                
+                present(alertController, animated: true, completion: nil)
+            } catch let error {
+                fatalError("\(error.localizedDescription)")
+            }
         }
     }
-
-
 }
 
